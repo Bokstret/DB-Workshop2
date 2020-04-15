2dda5384ea7b977def7f3ab1d2f0a44d9c235cdc
Bokstret <43569571+Bokstret@users.noreply.github.com>
2020-04-15 15:07:07 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -34,6 +34,6 @@ CREATE TABLE Collection(
     projectid NUMBER(20,0) NOT NULL PRIMARY KEY,
     collected NUMBER(20,0) NOT NULL,
     goal NUMBER(20,0) NOT NULL CHECK (goal > 0),
-    currancy VARCHAR(15) NOT NULL,
+    currency VARCHAR(15) NOT NULL,
     status VARCHAR(20) NOT NULL    
     );
