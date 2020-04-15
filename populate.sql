a379bb5dcc8460db784aa931ed243128d1159f6c
Bokstret <43569571+Bokstret@users.noreply.github.com>
2020-04-15 19:31:52 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -94,53 +94,55 @@ VALUES('failed');
 INSERT INTO collectionstatus(status)
 VALUES('successful');
 
+
 ----------------------------
 -- Populate collectiontime table
 ----------------------------
-INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000002330, '2015-08-11 12:12:28','2015-10-09 11:36:00');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000004038, '2013-01-12 00:20:50','2013-02-26 00:20:50');
+VALUES(1000002330, '11-08-2015 12:12:28','09-10-2015 11:36:00');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000007540, '2013-01-12 00:20:50','2013-02-26 00:20:50');
+VALUES(1000004038, '12-01-2013 00:20:50','26-02-2013 00:20:50');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(100004721, '2013-09-09 18:19:37','2013-10-09 18:19:37');
+VALUES(1000007540, '17-03-2012 03:24:11','16-04-2012 04:24:11');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000014025, '2016-02-26 13:38:27','2016-04-01 13:38:27');
+VALUES(100004721, '09-09-2013 18:19:37','09-10-2013 18:19:37');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000232288, '2015-05-24 12:47:52','2015-06-23 12:47:52');
+VALUES(1000014025, '26-02-2016 13:38:27','01-04-2016 13:38:27');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000256760, '2015-07-08 21:46:53','2015-08-07 21:46:53');
+VALUES(1000232288, '24-05-2015 12:47:52','23-06-2015 12:47:52');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000335422, '2014-06-10 00:09:47','2014-07-10 00:09:47');
+VALUES(1000256760, '08-07-2015 21:46:53','07-08-2015 21:46:53');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000190427, '2013-06-19 03:16:33','2013-07-19 03:16:33');
+VALUES(1000335422, '10-06-2014 00:09:47','10-07-2014 00:09:47');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000115172, '2015-03-01 18:06:32','2015-03-31 18:06:32');
+VALUES(1000190427, '19-06-2013 03:16:33','19-07-2013 03:16:33');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000104953, '2015-12-29 16:59:29','2016-01-28 16:59:29');
+VALUES(1000115172, '01-03-2015 18:06:32','31-03-2015 18:06:32');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000071625, '2012-05-03 17:24:32','2012-06-02 17:24:32');
+VALUES(1000104953, '29-12-2015 16:59:29','28-01-2016 16:59:29');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000320473, '2013-08-15 11:16:24','2013-09-13 08:55:00');
+VALUES(1000071625, '03-05-2012 17:24:32','02-06-2012 17:24:32');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000542390, '2015-03-01 05:52:30','2015-03-31 05:52:30');
+VALUES(1000320473, '15-08-2013 11:16:24','13-09-2013 08:55:00');
 
 INSERT INTO collectiontime(projectid,launched,deadline)
-VALUES(1000567557, '2012-02-24 02:52:41','2012-04-09 02:52:41');
+VALUES(1000542390, '01-03-2015 05:52:30','31-03-2015 05:52:30');
+
+INSERT INTO collectiontime(projectid,launched,deadline)
+VALUES(1000567557, '24-02-2012 02:52:41','09-04-2012 02:52:41');
 
 ----------------------------
 -- Populate collection table
